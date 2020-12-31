67177e046a36ba34a1a028afc3cac81cc7bcf1c4
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-31 21:07:51 +0700

    Wrap the widget with flat button

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -7,37 +7,40 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Wisata Bandung'),
       ),
-      body: Card(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Image.asset('images/farm-house.jpg'),
-            ),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      'Farm House Lembang',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text('Lembang')
-                  ],
-                ),
+      body: FlatButton(
+        onPressed: () {},
+        child: Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Image.asset('images/farm-house.jpg'),
               ),
-            )
-          ],
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        'Farm House Lembang',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('Lembang')
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
