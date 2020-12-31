f97d9d619deefa794403e8e43f853c702bed7d0f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-31 19:11:41 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/erikriosetiawan/my_sites_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.erikriosetiawan.my_sites_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Sites App',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Sites App'),
+        ),
+        body: Container(
+          child: Center(
+            child: Text('Hello, World!'),
+          ),
+        ),
+      ),
+    );
+  }
+}
